bc70a251e96278462aa35afb7a69c20bc8c54a0e
Kevin Buhler <buhlerkw@gmail.com>
2023-01-21 20:20:38 -0700

    got orbs to show on video

--- slam.py
+++ slam.py
@@ -1,13 +1,44 @@
-import cv2 as cv
+import numpy as np
+import cv2
 import sys
+import os
+
+W = 640
+H = 360
 
 if __name__ == "__main__":
-    print("Hello, World!")
+    cap = cv2.VideoCapture('test_countryroad.mp4')
 
-    img = cv.imread(cv.samples.findFile("starry-night.jpeg"))
+    W = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    H = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-    if img is None:
-        sys.exit("Could not read the image.")
+    # print(W, H)
 
-    cv.imshow("Display window", img)
-    k = cv.waitKey(0)  # will keep screen up until you press a key
+    # if os.getenv("ENV") is None:
+    #     print("got env env")
+
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        frame = cv2.resize(frame, (W//3, H//3))
+        # print(f"r ----- frame:{frame.shape}")
+
+        # Initiate STAR detector
+        orb = cv2.ORB_create()
+
+        # find the keypoints with ORB
+        kp = orb.detect(frame, None)
+
+        # compute the descriptors with ORB
+        kp, des = orb.compute(frame, kp)
+
+        # draw only keypoints location,not size and orientation
+        final_frame = cv2.drawKeypoints(
+            frame, kp, outImage=None, color=(0, 255, 0), flags=0)
+
+        cv2.imshow('final', final_frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
